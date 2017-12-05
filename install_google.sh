eef2e3141f65b212c10b4b829d711575b01c2191
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-12-05 20:58:21 +0700

    nothnig

--- install_google.sh
+++ install_google.sh
@@ -2,11 +2,11 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y python3-pip
-sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev -y
 sudo apt-get install python3-bs4 -y
 
 sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
-sudo apt-get install python3-pip
+sudo apt-get install python3-pip -y
 sudo pip3 install --upgrade pip
 sudo pip3 install selenium
 sudo pip install xvfbwrapper
@@ -22,5 +22,5 @@ sudo chmod 0755 /usr/local/bin/chromedriver
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update 
-sudo apt-get install google-chrome-stable
+sudo apt-get install google-chrome-stable -y
 
